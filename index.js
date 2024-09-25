8061dc919fd6c2af1d24157bffad946aecf1da57
Manu88x <emmanuelokoth112@gmail.com>
2024-09-25 18:42:55 +0300

    one

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+const companyName = "Scuber"; // For the companyName test
+let mostProfitableNeighborhood = "Chelsea"; // Make sure this line is present
+let companyCeo = "Susan Smith"; // Make sure this line is present
